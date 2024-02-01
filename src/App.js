2beba9341edbd51685b21bf4dd8ec76df9691794
sarthakbhatt1407 <sarthakbhatt1407@gmail.com>
2024-02-01 13:14:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import QrReader from "react-qr-scanner";
+const App = () => {
+  const [data, setData] = useState("No result");
+  const [showScan, setShowScan] = useState(false);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          setShowScan(!showScan);
+        }}
+      >
+        Scan to login
+      </button>
+      {showScan && (
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          style={{ width: "100%" }}
+        />
+      )}
+      <p>{data}</p>
     </div>
   );
-}
+};
 
 export default App;
