32db05e57d91121e0a14dea1c78c8f8183147431
sarthakbhatt1407 <sarthakbhatt1407@gmail.com>
2024-02-01 15:10:55 +0530

    first commit 1

--- src/App.js
+++ src/App.js
@@ -47,17 +47,19 @@ const App = () => {
               setData(resArr);
               setShowScan(!showScan);
               const onSubmitHandler = async (e) => {
-                const res = await fetch(`http://localhost:5000/user/login`, {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify(obj),
-                });
+                const res = await fetch(
+                  `https://hotelsolution-backend.vercel.app/user/login`,
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(obj),
+                  }
+                );
                 const data = await res.json();
                 console.log(data);
                 if (res.ok) {
-                  alert(data.message);
                   setData("Login Successfull");
                 }
                 if (!res.ok) {
