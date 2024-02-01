36ebdc295a6a8fbb2af69a40ad18d2e69ee30018
sarthakbhatt1407 <sarthakbhatt1407@gmail.com>
2024-02-01 14:50:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ const App = () => {
   const [data, setData] = useState("No result");
   const [showScan, setShowScan] = useState(false);
 
+  const onSubmitHandler = async (e) => {
+    const res = await fetch(`http://localhost:5000/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: data.userId,
+        password: data.password,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) {
+    }
+    if (!res.ok) {
+    }
+  };
+
   return (
     <div>
       <button
@@ -19,8 +38,30 @@ const App = () => {
           {" "}
           <QrScanner
             onDecode={(result) => {
-              setData(result);
+              const resArr = result.split(",");
+              const obj = {
+                userId: resArr[0].trim(),
+                password: resArr[1].trim(),
+              };
+              console.log(obj);
               setShowScan(!showScan);
+              const onSubmitHandler = async (e) => {
+                const res = await fetch(`http://localhost:5000/user/login`, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(obj),
+                });
+                const data = await res.json();
+                console.log(data);
+                if (res.ok) {
+                  setData("Login Successfull");
+                }
+                if (!res.ok) {
+                }
+              };
+              onSubmitHandler();
             }}
             onError={(error) => console.log(error?.message)}
           />
